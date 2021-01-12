d544fed032c0cdb6d44905d19cd3387e9c54ab6a
wangsiting <214325747@qq.com>
2021-01-12 19:53:52 +0800

    create project

---
+++ src/main/java/com/lanterntown/learnning/LearnningApplication.java
@@ -0,0 +1,13 @@
+package com.lanterntown.learnning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnningApplication.class, args);
+    }
+
+}
